3f484a8aeba15dda9c555dba7eb9073fb48ca996
Sahil Malik <sahilmalikroorkee@gmail.com>
2018-04-30 13:18:33 +0530

    Add files via upload

--- lanes.cpp
+++ lanes.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace cv;
 int main() {
+	const  Scalar WHITE = Scalar(255, 255, 255);
 	string file_name = "e:/solidYellowLeft.mp4";
 	VideoCapture cap(file_name);
 	system("cls");
@@ -20,7 +21,7 @@ int main() {
 		if (frame.empty())	break;
 		cvtColor(frame,gray,CV_BGR2GRAY);
 		equalizeHist(gray, gray);
-		dilate(gray, gray,kernel,Point(-1,-1) ,4);
+		//dilate(gray, gray,kernel,Point(-1,-1) ,4);
 		threshold(gray, gray, 128, 255, THRESH_OTSU);
 		vector<Point> points;
 		points.push_back(Point(0,gray.rows));
@@ -37,7 +38,7 @@ int main() {
 		vector<Vec2f> lines;
 		Canny(mask, mask, 50, 200, 3);
 		imshow("Preprocessed", mask);
-		HoughLines(mask,lines,2.3,CV_PI/180,150,0,0);
+		HoughLines(mask,lines,6,CV_PI/60,160,40,25);
 		for (size_t i = 0; i < lines.size(); i++)
 		{
 			float rho = lines[i][0], theta = lines[i][1];
@@ -48,7 +49,7 @@ int main() {
 			pt1.y = cvRound(y0 + 1000 * (a));
 			pt2.x = cvRound(x0 - 1000 * (-b));
 			pt2.y = cvRound(y0 - 1000 * (a));
-			line(frame, pt1, pt2, Scalar(rand()%255, 255, 255), 3, CV_AA);
+			line(frame, pt1, pt2,WHITE , 1, CV_AA);
 		}
 		imshow("test", frame);
 		waitKey(2);
